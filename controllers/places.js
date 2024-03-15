1183e8c619da5644b8897f9a05f9f031e8b6f972
madebygeelou <125709909+madebygeelou@users.noreply.github.com>
2024-03-14 19:46:40 -0700

    adding bootstrap grid

--- controllers/places.js
+++ controllers/places.js
@@ -6,13 +6,13 @@ router.get('/', (req, res) => {
         city: 'Seattle',
         state: 'WA',
         cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/images/edward-howell-hVtjc8qoQgg-unsplash.jpg'
       }, {
         name: 'Coding Cat Cafe',
         city: 'Phoenix',
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/images/rohan-g--mwNJswDlXE-unsplash.jpg'
       }]
     res.render('places/index', {places})
 })
--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,16 @@ function home () {
         <Def>
             <main>
                 <h1>HOME</h1>
+                <div>
+                    <img src="/images/mohamed-nohassi-odxB5oIG_iA-unsplash.jpg" alt="Man on the Beach"/>
+                    <div>
+                        Photo by <a href="https://unsplash.com/@coopery?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Mohamed Nohassi</a>
+                        on <a href="https://unsplash.com/s/photos/free">Unsplash</a>
+                    </div>
+                </div>
+                <a href="/places">
+  <button className="btn-primary">Places Page</button>
+</a>
             </main>
         </Def>
     )
