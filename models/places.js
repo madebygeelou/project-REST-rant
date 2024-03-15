418b9f083874992c0ba23839507fd10fb6d730e9
madebygeelou <125709909+madebygeelou@users.noreply.github.com>
2024-03-14 20:52:47 -0700

    finished part 4

---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/edward-howell-hVtjc8qoQgg-unsplash.jpg'
+  }, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/rohan-g--mwNJswDlXE-unsplash.jpg'
+  }]
