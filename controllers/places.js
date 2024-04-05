b4ba4477ccd7eaeed175898f4cad8551dd2a3e0f
madebygeelou <125709909+madebygeelou@users.noreply.github.com>
2024-04-04 19:45:15 -0700

    made changes to the problems

--- controllers/places.js
+++ controllers/places.js
@@ -2,37 +2,30 @@ const router = require('express').Router()
 const db = require('../models')
 
 router.get('/', (req, res) => {
-    db.Place.find()
+  db.Place.find()
     .then((places) => {
-      res.render('places/index', { places })
+    res.render('places/index', { places })
     })
     .catch(err => {
-      console.log(err) 
-      res.render('error404')
-    })
+    console.log(err);
+    res.render('error404')
+  })
 })
 
 router.post('/', (req, res) => {
+  if (!req.body.pic) {
+    req.body.pic = 'http://placekitten/400/400'
+  }
   db.Place.create(req.body)
-  .then(() => {
+    .then(() => {
       res.redirect('/places')
-  })
-  .catch(err => {
-      if (err && err.name == 'ValidationError') {
-let message= 'Validation Error:'
-for(var field in err.errors) {
-  message += '${field} was ${err.errors[field].value}.'
-  message += '${err.errors[field].message}'
-}
-console.log('Validation Error message', message)
-res.render('places/new', { message })
-      }
-      else {
+    })
+    .catch(err => {
+      console.log('err', err);
       res.render('error404')
-      }
-  })
+    })
+  res.send('POST /places stub')
 })
-
 
 router.get('/new', (req, res) => {
   res.render('places/new')
@@ -40,32 +33,38 @@ router.get('/new', (req, res) => {
 
 router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
-  .populate('comments')
-  .then(place => {
-    console.log(place.comments)
+    .populate('comments')
+    .then(place => {
+      console.log([place.comments]);
       res.render('places/show', { place })
-  })
-  .catch(err => {
+    })
+    .catch(err => {
       console.log('err', err)
       res.render('error404')
-  })
+    })
 })
 
 
 router.put('/:id', (req, res) => {
   db.Place.findByIdAndUpdate(req.params.id, req.body)
-  .then(() => {
-      res.redirect(`/places/${req.params.id}`)
-  })
-  .catch(err => {
-      console.log('err', err)
+    .then(() => {
+      res.redirect(`/places${req.params.id}`)
+    })
+    .catch(err => {
+      console.log('err', err);
       res.render('error404')
   })
 })
 
-
 router.delete('/:id', (req, res) => {
-  res.send('DELETE /places/:id stub')
+  db.Place.findByIdAndDelete(req.params.id, req.body)
+  .then((place) => {
+    res.redirect(`/places${req.params.id}`)
+  })
+  .catch(err => {
+    console.log('err', err);
+    res.render('error404')
+})
 })
 
 router.get('/:id/edit', (req, res) => {
@@ -78,43 +77,49 @@ router.get('/:id/edit', (req, res) => {
   })
 })
 
+router.post('/:id/comment', (req, res) => {
+  console.log('post comment', req.body)
+  if (req.body.author === '') { req.body.author = undefined }
+  req.body.rant = req.body.rant ? true : false
+  db.Place.findById(req.params.id)
+      .then(place => {
+          db.Comment.create(req.body)
+              .then(comment => {
+                  place.comments.push(comment.id)
+                  place.save()
+                      .then(() => {
+                          res.redirect(`/places/${req.params.id}`)
+                      })
+                      .catch(err => {
+                          res.render('error404')
+                      })
+              })
+              .catch(err => {
+                  res.render('error404')
+              })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
+})
 
-router.post('/:id/rant', (req, res) => {
-  res.send('GET /places/:id/rant stub')
+router.delete('/:id/rant/:rantId', (req, res) => {
+  res.send('GET /places/:id/rant/:rantId stub')
 })
 
 router.delete('/:id', (req, res) => {
-  db.Place.findByIdAndDelete(req.params.id)
-  .then(place => {
-      res.redirect('/places')
-  })
-  .catch(err => {
-      console.log('err', err)
-      res.render('error404')
-  })
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    places.splice(id, 1)
+    res.redirect('/places')
+  }
 })
 
-router.post('/:id/comment', (req,res) => {
-  console.log(req.body)
-  req.body.rant = req.body.rant ? true : false
-  db.Place.findById(req.params.id)
-  .then(place => {
-    db.Comment.create(req.body)
-    .then(comment => {
-      place.comments.push(comment.id)
-      place.save()
-      .then (() => {
-        res.redirect('/places/${req.params.id')
-      })
-    })
-    .catch(err => {
-      res.render('error404')
-    })
-  })
-  .catch(err=> {
-    res.render('error404')
-  })
-  res.send('GET /places/:id/comment stub')
-})
 
 module.exports = router
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,39 +1,36 @@
 const React = require('react')
 const Def = require('../default')
 
-function index (data) {
+function index(data) {
     let placesFormatted = data.places.map((place) => {
-      return (
-        <div className="col-sm-6">
-          <h2>{place.name}</h2>
-          <a href={'/places/${place.id}'}>
-            {place.name}
-          </a>
-          <p className='text-center'>
-            {place.cuisines}
-          </p>
-          <img src={place.pic} alt={place.name}/>
-          <p className='text-center'>
-            Located in {place.city}, {place.state}
-          </p>
-        </div>
-      )
+        return (
+            <div className='col-sm-6'>
+                <h2>
+                    <a href={`/places/${place.id}`}>
+                        {place.name}
+                    </a>
+                </h2>
+                <p className='text-center'>
+                    {place.cuisines}
+                </p>
+                <img src={place.pic} alt={place.name} />
+                <p className='text-center'>
+                    Located in {place.city}, {place.state}
+                </p>
+            </div>
+        )
     })
     return (
-      <Def>
-          <main>
-              <h1>Places to Rant About</h1>
-              <div className="row">
-              <div className="form-group">
-  <label for="founded">Founded Year</label>
-  <input className="form-control" id="founded" name="founded" />
-</div>
-              {placesFormatted}
-              </div>
-          </main>
-      </Def>
-  )
-  }
+        <Def>
+            <main>
+                <h1>PLACES INDEX!</h1>
+                <div className='row'>
+                    {placesFormatted}
+                </div>
+            </main>
+        </Def>
+    )
+}
 
-  module.exports = index
-  
+
+module.exports = index
